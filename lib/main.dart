8e485c00430fdbf5d8719fd9fd00a83cd7209b83
Anthony <anthonyghanem5@gmail.com>
2025-01-16 13:34:43 +0200

    Added Cubit State Management and Managed the states correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scholar_chat/firebase_options.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
+import 'package:scholar_chat/pages/cubits/chat_cubit/cubit/chat_cubit.dart';
+import 'package:scholar_chat/pages/cubits/login_cubit/login_cubit.dart';
+import 'package:scholar_chat/pages/cubits/register_cubit/cubit/register_cubit.dart';
 import 'package:scholar_chat/pages/login_page.dart';
 import 'package:scholar_chat/pages/register_page.dart';
 
@@ -18,13 +22,22 @@ class ScholarChat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        LoginPage.id: (context) => LoginPage(),
-        RegisterPage.id: (context) => RegisterPage(),
-        ChatPage.id: (context) => ChatPage(),
-      },
-      initialRoute: LoginPage.id,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(
+          create: (context) => RegisterCubit(),
+        ),
+        BlocProvider(create: (context) => ChatCubit())
+      ],
+      child: MaterialApp(
+        routes: {
+          LoginPage.id: (context) => LoginPage(),
+          RegisterPage.id: (context) => RegisterPage(),
+          ChatPage.id: (context) => ChatPage(),
+        },
+        initialRoute: LoginPage.id,
+      ),
     );
   }
 }
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scholar_chat/constants.dart';
 import 'package:scholar_chat/models/message.dart';
+import 'package:scholar_chat/pages/cubits/chat_cubit/cubit/chat_cubit.dart';
 import 'package:scholar_chat/widgets/chat_bubble.dart';
 
 // ignore: must_be_immutable
@@ -9,90 +10,83 @@ class ChatPage extends StatelessWidget {
   static String id = 'ChatPage';
 
   final _controller = ScrollController();
+  List<Message> messagesList = [];
 
-  CollectionReference messages =
-      FirebaseFirestore.instance.collection(kMessagesCollections);
   TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     String email = ModalRoute.of(context)!.settings.arguments as String;
-    return StreamBuilder<QuerySnapshot>(
-        stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<Message> messagesList = [];
-            for (int i = 0; i < snapshot.data!.docs.length; i++) {
-              messagesList.add(Message.fromJson(snapshot.data!.docs[i]));
-            }
-            return Scaffold(
-              appBar: AppBar(
-                automaticallyImplyLeading: false, // for the return arrow
-                backgroundColor: kPrimaryColor,
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      kLogo,
-                      height: 50,
-                    ),
-                    const Text('Scholar Chat'),
-                  ],
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // for the return arrow
+        backgroundColor: kPrimaryColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              kLogo,
+              height: 50,
+            ),
+            const Text('Scholar Chat'),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: BlocConsumer<ChatCubit, ChatState>(
+              listener: ((context, state) => {
+                    if (state is ChatSuccess)
+                      {
+                        messagesList = state.messages,
+                      }
+                  }),
+              builder: (context, state) {
+                return ListView.builder(
+                    reverse: true,
+                    controller: _controller,
+                    itemCount: messagesList.length,
+                    itemBuilder: (context, index) {
+                      return messagesList[index].id == email
+                          ? ChatBubble(
+                              message: messagesList[index],
+                            )
+                          : ChatBubbleForFriend(message: messagesList[index]);
+                    });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              controller: controller,
+              onSubmitted: (data) {
+                BlocProvider.of<ChatCubit>(context)
+                    .sendMessage(message: data, email: email);
+                controller.clear();
+                _controller.animateTo(0,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn);
+              },
+              decoration: InputDecoration(
+                hintText: 'Send Message',
+                suffix: const Icon(
+                  Icons.send,
+                  color: kPrimaryColor,
                 ),
-                centerTitle: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: const BorderSide(color: kPrimaryColor)),
               ),
-              body: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                        reverse: true,
-                        controller: _controller,
-                        itemCount: messagesList.length,
-                        itemBuilder: (context, index) {
-                          return messagesList[index].id == email
-                              ? ChatBubble(
-                                  message: messagesList[index],
-                                )
-                              : ChatBubbleForFriend(
-                                  message: messagesList[index]);
-                        }),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: TextField(
-                      controller: controller,
-                      onSubmitted: (data) {
-                        messages.add({
-                          kMessage: data,
-                          kCreatedAt: DateTime.now(),
-                          'id': email,
-                        });
-                        controller.clear();
-                        _controller.animateTo(0,
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.fastOutSlowIn);
-                      },
-                      decoration: InputDecoration(
-                        hintText: 'Send Message',
-                        suffix: const Icon(
-                          Icons.send,
-                          color: kPrimaryColor,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: kPrimaryColor)),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          } else {
-            return const Text('Data is still loading, please wait');
-          }
-        });
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
